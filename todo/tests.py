8e880a34c603869ca03cecfab1a142dbfcb4b7d9
Nicolas Varas <nwvaras@gmail.com>
2018-03-23 16:04:02 +0000

    Added new (failing) tests

--- todo/tests.py
+++ todo/tests.py
@@ -5,21 +5,48 @@ from models import TodoList
 class YourTestClass(TestCase):
 
     def test_empty_list(self):
-        #arrange
+        #given
         
-        #act
+        #when
         todoList = TodoList()
-        #assert
+        #then
         self.assertTrue(todoList.is_empty())
         
     def test_add_task(self):
-        #arrange
+        #given
         todoList = TodoList()
         
-        #act
+        #when
         todoList.add_task('afeitar al gato')
         
-        #assert
+        #then
         self.assertFalse(todoList.is_empty())
         self.assertEquals('afeitar al gato', todoList.first_task())
+        
+        
+    def test_list_tasks(self):
+        #given
+        todoList = TodoList()
+        todoList.add_task('afeitar al gato')
+        
+        #when
+        
+    
+        #then
+        self.assertTrue(False)
+    
+    def test_add_many_tasks(self):
+        #given
+        todoList = TodoList()
+        todoList.add_task('afeitar al gato')
+    
+        #when
+        todoList.add_task('Afeitar al perro')
+        todoList.add_task('Peinar la mugneca')
+    
+        #then
+        self.assertEqual(3, todoList.length())
+        self.assertTrue(todoList.contains('afeitar al gato'))
+        self.assertTrue(todoList.contains('Afeitar al perro'))
+        self.assertTrue(todoList.contains('Peinar la mugneca'))
         
